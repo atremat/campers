926bd9d2203250373e1b22a1c7b0c05c2d66fb63
Artem <atremat@gmail.com>
2024-08-15 23:19:23 +0300

    render list and icons

---
+++ src/components/CamperItemOptions/CamperItemOptions.jsx
@@ -0,0 +1,77 @@
+import Icon from '../Icon/Icon';
+import css from './CamperItemOptions.module.css';
+
+export const CamperItemOptions = ({ item }) => {
+  console.log(item);
+
+  return (
+    <ul className={css.list}>
+      <li className={css.item}>
+        <Icon
+          id="adults"
+          width={20}
+          height={20}
+          className={css.icon}
+          fillColor={'#101828'}
+        />
+        {item.adults} adults
+      </li>
+      <li className={css.item}>
+        <Icon
+          id="transmission"
+          width={20}
+          height={20}
+          className={css.icon}
+          fillColor={'#101828'}
+        />
+        {item.transmission.charAt(0).toUpperCase() +
+          item.transmission.slice(1).toLowerCase()}
+      </li>
+      <li className={css.item}>
+        <Icon
+          id="fuel"
+          width={20}
+          height={20}
+          className={css.icon}
+          fillColor={'#101828'}
+        />
+        {item.engine.charAt(0).toUpperCase() +
+          item.engine.slice(1).toLowerCase()}
+      </li>
+      {item.details.kitchen && (
+        <li className={css.item}>
+          <Icon
+            id="kitchen"
+            width={20}
+            height={20}
+            className={css.icon}
+            fillColor={'#101828'}
+          />
+          Kitchen
+        </li>
+      )}
+      <li className={css.item}>
+        <Icon
+          id="bed"
+          width={20}
+          height={20}
+          className={css.icon}
+          fillColor={'#101828'}
+        />
+        {item.details.beds} beds
+      </li>
+      {item.details.airConditioner && (
+        <li className={css.item}>
+          <Icon
+            id="ac-converted"
+            width={20}
+            height={20}
+            className={css.icon}
+            fillColor={'#101828'}
+          />
+          AC
+        </li>
+      )}
+    </ul>
+  );
+};
--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -1,5 +1,8 @@
 import css from './CatalogItem.module.css';
 
+import { CamperItemOptions } from '../CamperItemOptions/CamperItemOptions';
+import Icon from '../Icon/Icon';
+
 export const CatalogItem = ({ item }) => {
   return (
     <li className={css.item}>
@@ -8,10 +11,27 @@ export const CatalogItem = ({ item }) => {
         <div className={css.mainInfo}>
           <div className={css.nameWrapper}>
             <h3 className={css.name}>{item.name}</h3>
-            <h3 className={css.price}>€ {item.price.toFixed(2)}</h3>
+            <div className={css.priceWrapper}>
+              <h3 className={css.price}>€ {item.price.toFixed(2)}</h3>
+              <Icon
+                id="like-new"
+                className={css.like}
+                width={24}
+                height={24}
+                fillColor="#101828"
+              />
+            </div>
           </div>
 
           <div className={css.ratingWrapper}>
+            <Icon
+              id="star-empty"
+              className={css.star}
+              width={16}
+              height={16}
+              fillColor="#ffc531"
+            />
+
             <p
               className={css.rating}
             >{`${item.rating} (${item.reviews.length} Reviews)`}</p>
@@ -21,7 +41,22 @@ export const CatalogItem = ({ item }) => {
 
         <p className={css.aboutText}>{item.description}</p>
 
-        <div className={css.additionalInfo}></div>
+        <CamperItemOptions item={item} />
+        {/* <div className={css.additionalInfo}>
+          {Object.entries(item.details)
+            .filter(
+              ([key, value]) => value !== null && value !== '' && value != 0
+            )
+            .map(([key, value]) => (
+              <span key={key} className={css.option}>
+                {key}: {value}
+              </span>
+            ))}
+        </div> */}
+
+        <button type="button" className={css.moreBtn}>
+          Show more
+        </button>
       </div>
     </li>
   );
